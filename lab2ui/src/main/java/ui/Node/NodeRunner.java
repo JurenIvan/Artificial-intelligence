63454a02e40c65bebda3eb0a0b7784984e664feb
jurenivan <juren.ivan@gmail.com>
2020-04-23 02:15:32 +0200

    modify runner

--- lab2ui/src/main/java/ui/Node/NodeRunner.java
+++ lab2ui/src/main/java/ui/Node/NodeRunner.java
@@ -1,13 +1,28 @@
 package ui.Node;
 
-import java.util.Arrays;
+import ui.parser.Parser;
 
 public class NodeRunner {
 
+    private static final String input = "A v B \n" +
+            "~A v B \n" +
+            "(~A v B) & (A v ~B) \n" +
+            "A & B \n" +
+            "A & ~B \n" +
+            "(~A v A) & (~A v ~B) & (B v A) & (B v ~B) \n" +
+            "((A v B) v ~C) & ((A v B) v ~D) & ((~A v ~B) v ~C) & ((~A v ~B) v ~D) \n" +
+            "((A v B) v ~C) & ((A v B) v ~D) & ((~A v ~B) v ~C) & ((~A v ~B) v ~D) \n" +
+            "((C v D) v (A v B)) & ((C v D) v (~A v ~B)) \n" +
+            "~C & ~D & (A v ~A) & (A v ~B) & (B v ~A) & (B v ~B) \n" +
+            "((C v D) v (A v B)) & ((C v D) v (~A v ~B)) ";
+
     public static void main(String[] args) {
 
-        Node root = new NodeAnd(new NodeData("stela"), new NodeImplication(new NodeEquivalence(new NodeData("fortnite",true),new NodeData("panda")),new NodeData("Pubg")));
+        String[] lines = input.split("\n");
+        for (var line : lines) {
+            Parser parser = new Parser();
+            System.out.println(parser.parse(line).convert().getValue());
+        }
 
-        System.out.println(Arrays.asList(root.convert().getValue().split("\n")));
     }
 }
